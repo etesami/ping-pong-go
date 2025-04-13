ded9eea73e539309d5668403ad9c6bbcece9f7b6
Ehsan <eh.etesami@gmail.com>
2025-04-13 03:35:36 +0000

    Update ping and pong file

---
+++ ping-go/cmd/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	api "github.com/etesami/ping-pong-go/api"
+	pb "github.com/etesami/ping-pong-go/pkg/protoc"
+
+	// "github.com/prometheus/client_golang/prometheus/promhttp"
+	// "google.golang.org/genproto/googleapis/api/metric"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func generateRandomBytes(sizeMB int64) ([]byte, error) {
+	size := sizeMB * 1024 * 1024
+	randomBytes := make([]byte, size)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+	return randomBytes, nil
+}
+
+func sendData(client pb.MessageClient, sizeMB int64) error {
+	randomBytes, err := generateRandomBytes(sizeMB)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	sentTimestamp := time.Now()
+	ack, err := client.SendData(ctx, &pb.Data{
+		Payload:       randomBytes,
+		SentTimestamp: fmt.Sprintf("%d", int(sentTimestamp.UnixMilli())),
+	})
+	if err != nil {
+		return fmt.Errorf("send data not successful: %v", err)
+	}
+	fileSize := float64(len(randomBytes)) / (1024 * 1024)
+	log.Printf("Sent [%.2f] bytes. Ack recevied, status: [%s]\n", fileSize, ack.Status)
+
+	return nil
+}
+
+func sendDataInit(client pb.MessageClient, fileSize string) {
+	if fileSize == "" {
+		log.Println("FILE_SIZE environment variable not set. Using default value of 1 MB.")
+		fileSize = "1"
+	}
+	fileSizeInt, err := strconv.ParseInt(fileSize, 10, 64)
+	if err != nil {
+		log.Fatalf("Invalid FILE_SIZE value: %v. Using default value of 1MB.", err)
+		fileSizeInt = 1
+	}
+	sendData(client, fileSizeInt)
+}
+
+func main() {
+	// Target service initialization
+	svcTargetAddress := os.Getenv("SVC_ADDR")
+	svcTargetPort := os.Getenv("SVC_PORT")
+
+	targetSvc := &api.Service{
+		Address: svcTargetAddress,
+		Port:    svcTargetPort,
+	}
+
+	var conn *grpc.ClientConn
+	var client pb.MessageClient
+	go func() {
+		for {
+			var err error
+			conn, err = grpc.NewClient(
+				targetSvc.Address+":"+targetSvc.Port,
+				grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				log.Printf("Failed to connect to target service: %v", err)
+				continue
+			}
+			client = pb.NewMessageClient(conn)
+			log.Printf("Connected to target service: %s:%s\n", targetSvc.Address, targetSvc.Port)
+			return
+		}
+	}()
+	defer conn.Close()
+
+	fileSize := os.Getenv("FILE_SIZE")
+	updateFrequencyStr := os.Getenv("UPDATE_FREQUENCY")
+
+	if updateFrequencyStr == "" {
+		log.Println("UPDATE_FREQUENCY environment variable not set. Sending data and exit.")
+		sendDataInit(client, fileSize)
+		return
+	}
+
+	updateFrequency, err := strconv.Atoi(updateFrequencyStr)
+	if err != nil {
+		log.Fatalf("Error parsing update frequency: %v", err)
+	}
+	ticker := time.NewTicker(time.Duration(updateFrequency) * time.Second)
+	defer ticker.Stop()
+
+	// Send data initially
+	sendDataInit(client, fileSize)
+
+	go func(c *pb.MessageClient) {
+		for range ticker.C {
+			sendDataInit(*c, fileSize)
+		}
+	}(&client)
+
+	// metricAddr := os.Getenv("METRIC_ADDR")
+	// metricPort := os.Getenv("METRIC_PORT")
+	// http.Handle("/metrics", promhttp.Handler())
+	// log.Printf("Starting server on :%s\n", metricPort)
+	// http.ListenAndServe(fmt.Sprintf("%s:%s", metricAddr, metricPort), nil)
+}
---
+++ pong-go/cmd/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+
+	api "github.com/etesami/ping-pong-go/api"
+	pb "github.com/etesami/ping-pong-go/pkg/protoc"
+
+	// "github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.UnimplementedMessageServer
+}
+
+func (s Server) SendData(ctx context.Context, recData *pb.Data) (*pb.Ack, error) {
+	st := time.Now()
+	recTimestamp := st.UnixMilli()
+	log.Printf("Received at [%s]: [%d]\n", st.Format("2006-01-02 15:04:05"), len(recData.Payload))
+
+	ack := &pb.Ack{
+		Status:                "ok",
+		OriginalSentTimestamp: recData.SentTimestamp,
+		ReceivedTimestamp:     strconv.Itoa(int(recTimestamp)),
+		AckSentTimestamp:      strconv.Itoa(int(time.Now().UnixMilli())),
+	}
+	return ack, nil
+}
+
+func main() {
+
+	// Local service initialization
+	svcAddress := os.Getenv("SVC_ADDR")
+	svcPort := os.Getenv("SVC_PORT")
+
+	localSvc := &api.Service{
+		Address: svcAddress,
+		Port:    svcPort,
+	}
+
+	// We listen on all interfaces
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", localSvc.Port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterMessageServer(grpcServer, Server{})
+
+	go func() {
+		log.Printf("starting gRPC server on port %s:%s\n", localSvc.Address, localSvc.Port)
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// metricAddr := os.Getenv("METRIC_ADDR")
+	// metricPort := os.Getenv("METRIC_PORT")
+	// http.Handle("/metrics", promhttp.Handler())
+	// log.Printf("Starting server on :%s\n", metricPort)
+	// http.ListenAndServe(fmt.Sprintf("%s:%s", metricAddr, metricPort), nil)
+}
